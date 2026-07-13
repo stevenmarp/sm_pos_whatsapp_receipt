3236f863aa474423b86a0a61a48fad19192c59f2
stevenmarp <100309799+stevenmarp@users.noreply.github.com>
2026-07-14 02:08:48 +0700

    fix: make templates and javascript compatible with Odoo 19 OWL

--- sm_pos_whatsapp_receipt/static/src/receipt_screen/whatsapp_button.js
+++ sm_pos_whatsapp_receipt/static/src/receipt_screen/whatsapp_button.js
@@ -3,18 +3,10 @@
 import { _t } from "@web/core/l10n/translation";
 import { ReceiptScreen } from "@point_of_sale/app/screens/receipt_screen/receipt_screen";
 import { patch } from "@web/core/utils/patch";
-import { useService } from "@web/core/utils/hooks";
 
 patch(ReceiptScreen.prototype, {
-    setup() {
-        super.setup();
-        this.notification = useService("pos_notification");
-        const partner = this.currentOrder.get_partner();
-        this.orderUiState.inputPhone = this.orderUiState.inputPhone || (partner && partner.phone) || (partner && partner.mobile) || "";
-    },
-
     _whatsappCountryCode() {
-        const partner = this.currentOrder.get_partner();
+        const partner = this.currentOrder.getPartner();
         const country = partner?.country_id || this.pos.company.country_id;
         return (country?.phone_code || "").toString().replace(/\D/g, "");
     },
@@ -22,23 +14,23 @@ patch(ReceiptScreen.prototype, {
     _waReceiptText() {
         const order = this.currentOrder;
         const fmt = (val) => this.env.utils.formatCurrency(val);
-        const lines = order.get_orderlines().map(
-            (l) => `${l.get_quantity()} x ${l.get_full_product_name()}  ${fmt(l.get_all_prices().priceWithTax)}`
+        const lines = order.getOrderlines().map(
+            (l) => `${l.qty} x ${l.getFullProductName()}  ${fmt(l.priceIncl)}`
         );
         return [
             `*${this.pos.company.name}*`,
-            `${_t("Receipt")}: ${order.pos_reference || order.get_name()}`,
+            `${_t("Receipt")}: ${order.pos_reference || order.name}`,
             "--------------------",
             ...lines,
             "--------------------",
-            `${_t("Total")}: ${fmt(order.get_total_with_tax())}`,
+            `${_t("Total")}: ${fmt(order.priceIncl)}`,
             "",
             _t("Thank you for your purchase!"),
         ].join("\n");
     },
 
     actionSendReceiptOnWhatsApp() {
-        let phone = (this.orderUiState.inputPhone || "").replace(/\D/g, "");
+        let phone = (this.state.phone || "").replace(/\D/g, "");
         const cc = this._whatsappCountryCode();
         if (phone.startsWith("0") && !cc) {
             this.notification.add(
